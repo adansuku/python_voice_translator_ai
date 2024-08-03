e2e37c780dea0e302099d7a4b1630983c95c85de
Adán González <adangrx@gmail.com>
2024-08-03 10:51:58 +0100

    feat: added elevenlabs to add a speaker

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ import gradio as gr
 import whisper as wh
 from translate import Translator
 from dotenv import dotenv_values
+from elevenlabs.client import ElevenLabs
+from elevenlabs import VoiceSettings
+
 
 dotenv_values(".env")
+
+ELEVEN_API_KEY = dotenv_values(".env")["ELEVEN_API_KEY"]
 
 def tanslator(audio_file):
     try:
@@ -12,11 +17,37 @@ def tanslator(audio_file):
         transciption = result['text']
     except Exception as e:
         raise gr.error(
-            f"Error: {e}"
+            f"Error something went wrong creating the audio file: {e}"
         )
-    transciption = Translator(to_lang="en").translate(transciption)
-    print(transciption)
-    return transciption
+    
+    try:
+        transciption = Translator(to_lang="en").translate(transciption)
+    except Exception as e:
+        raise gr.error(
+            f"Error, something failed on translation: {e}"
+        )
+        
+    client = ElevenLabs(api_key=ELEVEN_API_KEY)
+    
+    response = client.text_to_speech.convert(
+            voice_id="pNInz6obpgDQGcFmaJgB",  # Adam pre-made voice
+            optimize_streaming_latency="0",
+            output_format="mp3_22050_32",
+            text=transciption,
+            model_id="eleven_turbo_v2",  
+            voice_settings=VoiceSettings(
+                stability=0.0,
+                similarity_boost=1.0,
+                style=0.0,
+                use_speaker_boost=True,
+            )
+        )
+
+    save_file_path = "audios/en.mp3"
+    with open(save_file_path, "wb") as file:
+        for chunk in response:
+            if chunk:
+                file.write(chunk)
 
 
 web = gr.Interface(

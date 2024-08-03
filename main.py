b16a78de1e3cde89dcede687ab025ce3d411b885
Adán González <adangrx@gmail.com>
2024-08-03 11:21:19 +0100

    feat: creating audio file

--- main.py
+++ main.py
@@ -13,50 +13,61 @@ ELEVEN_API_KEY = dotenv_values(".env")["ELEVEN_API_KEY"]
 def tanslator(audio_file):
     try:
         model = wh.load_model("base")
-        result = model.transcribe(audio_file)
-        transciption = result['text']
+        result = model.transcribe(audio_file, fp16=False)
+        transcription = result['text']
     except Exception as e:
-        raise gr.error(
+        raise gr.Error(
             f"Error something went wrong creating the audio file: {e}"
         )
     
+    print(f"transcription: {transcription}")
+    
     try:
-        transciption = Translator(to_lang="en").translate(transciption)
+        en_transciption = Translator(to_lang="en").translate(transcription)
     except Exception as e:
-        raise gr.error(
+        raise gr.Error(
             f"Error, something failed on translation: {e}"
         )
         
-    client = ElevenLabs(api_key=ELEVEN_API_KEY)
-    
-    response = client.text_to_speech.convert(
-            voice_id="pNInz6obpgDQGcFmaJgB",  # Adam pre-made voice
-            optimize_streaming_latency="0",
-            output_format="mp3_22050_32",
-            text=transciption,
-            model_id="eleven_turbo_v2",  
-            voice_settings=VoiceSettings(
-                stability=0.0,
-                similarity_boost=1.0,
-                style=0.0,
-                use_speaker_boost=True,
+    print(f"translation: {en_transciption}")
+        
+    try:
+        client = ElevenLabs(api_key=ELEVEN_API_KEY)
+        print(ELEVEN_API_KEY)
+        
+        response = client.text_to_speech.convert(
+                voice_id="Xb7hH8MSUJpSbSDYk0k2",  # A sweet girl :)
+                optimize_streaming_latency="0",
+                output_format="mp3_22050_32",
+                text=en_transciption,
+                model_id="eleven_turbo_v2",  
+                voice_settings=VoiceSettings(
+                    stability=0.0,
+                    similarity_boost=1.0,
+                    style=0.0,
+                    use_speaker_boost=True,
+                )
             )
+
+        save_file_path = "audios/en.mp3"
+        with open(save_file_path, "wb") as file:
+            for chunk in response:
+                if chunk:
+                    file.write(chunk)
+    except Exception as e:
+        raise gr.Error(
+            f"Error, something failed when the audio was created: {e}"
         )
-
-    save_file_path = "audios/en.mp3"
-    with open(save_file_path, "wb") as file:
-        for chunk in response:
-            if chunk:
-                file.write(chunk)
-
+        
+    return save_file_path
 
 web = gr.Interface(
-    fn=lambda x: x, 
+    fn=tanslator, 
     inputs=gr.Audio(
         sources=["microphone"], 
         type="filepath"
         ), 
-    outputs=[],
+    outputs=[gr.Audio(label="Translated Audio")],
     title= "Audio Transcription",
     description= "Transcribe audio from your microphone"
     )

a2bf4bdc05a374eb72bc7f07f068db97bdc9affe
Adán González <adangrx@gmail.com>
2024-08-03 10:18:47 +0100

    feat: Add whisper and translate to convert voice to text

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import gradio as gr
+import whisper as wh
+from tanslate import Translator
 
-def tanslator(audio_file)
-    
+def tanslator(audio_file):
+    try:
+        model = wh.load_model("base")
+        result = model.transcribe(audio_file)
+        transciption = result['text']
+    except Exception as e:
+        gr.error(
+            f"Error: {e}"
+        )
+    transciption
+
 
 web = gr.Interface(
     fn=lambda x: x, 
